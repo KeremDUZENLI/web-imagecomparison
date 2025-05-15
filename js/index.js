5d59ac7e503701caaa5214f9fad6c8f6a6d86454
KeremDUZENLI <krmdznl97@gmail.com>
2025-05-15 22:16:30 +0200

    feat: lowercase is operated in html

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ export async function initIntroForm() {
   form.onsubmit = async e => {
     e.preventDefault();
 
-    const name = input.value.trim().toLowerCase();
+    const name = input.value;
     const existingNames = await fetchExistingNames();
     if (existingNames.includes(name)) {
       return alert('This name has been taken');
